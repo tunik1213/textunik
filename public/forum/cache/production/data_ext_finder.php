09af1f53e7ca70fcf1d0d9fa2bb8845fd7787cf5
tunik <tunik@detali.zp.ua>
2019-10-12 08:51:17 -0400

    gitignore forum cache

--- public/forum/cache/production/data_ext_finder.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1601623644
-404
-a:4:{s:32:"d02806c98648acbc3e4c04be69a4e244";a:1:{i:0;a:4:{s:10:"named_path";s:50:"ext/phpbb/viglink/language/en/info_acp_viglink.php";s:8:"ext_name";s:13:"phpbb/viglink";s:4:"path";s:30:"ext/phpbb/viglink/language/en/";s:8:"filename";s:20:"info_acp_viglink.php";}}s:32:"9edf501a5755e0575c3e210c8f505815";a:0:{}s:32:"fe3f0a57fc39b546822dab2264a5120e";a:0:{}s:32:"fb8d6a9fce788c3bf9b5336bc52e31ae";a:0:{}}
