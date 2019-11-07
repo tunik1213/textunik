98f27502b145306e804207337363c1813d8fc97a
tunik <tunik@detali.zp.ua>
2019-11-07 09:03:14 -0500

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app');
+
+@section('content')
+    <div class="container">
+        <h1>Страница не найдена</h1>
+        <p>Запрашиваемая Вами страница не существует. Возможно, она устарела и была удалена.</p>
+
+        <div class="button">
+            <a href="/" class="button">Вернуться на главную</a>
+        </div>
+    </div>
+@endsection
+
