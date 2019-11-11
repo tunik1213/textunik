048e2ba453995a687a7542be528c5bc58a44d153
tunik <tunik@detali.zp.ua>
2019-11-11 07:42:26 -0500

    Прикручен форум chatter пока без верстки

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="MobileOptimized" content="320" />
-    <meta name="HandheldFriendly" content="true" />
+    <meta name="MobileOptimized" content="320"/>
+    <meta name="HandheldFriendly" content="true"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -28,7 +28,7 @@
 
     <!-- jQuery Modal -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css"/>
 
     @yield('head')
 
@@ -36,81 +36,89 @@
         <script src="{{ asset('js/guest.js') }}"></script>
     @endguest
 
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+
+    @yield('css');
+    @yield('js')
 
 </head>
 <body>
-    <div id="app">
-        <nav id="header" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="/images/Textile_logo.png" />
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav id="header" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="/images/Textile_logo.png"/>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            {{--                                <a class="nav-link" href="{{ route('login') }}">Вход</a>--}}
+                            <a class="nav-link" href="/login_form" rel="modal:open">Вход</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-{{--                                <a class="nav-link" href="{{ route('login') }}">Вход</a>--}}
-                                <a class="nav-link" href="/login_form" rel="modal:open">Вход</a>
+                                <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
-                                </li>
-                            @endif
-                        @else
-{{--                            @if (Auth::user()->moderator)--}}
-{{--                                <a href="/moderation/" class="btn">Модерация</a>--}}
-{{--                            @endif--}}
-                            <li>
-                                <a href="/article/add/" class="btn">Написать в блог</a>
-                            </li>
+                        @endif
+                    @else
+                        {{--                            @if (Auth::user()->moderator)--}}
+                        {{--                                <a href="/moderation/" class="btn">Модерация</a>--}}
+                        {{--                            @endif--}}
+                        <li>
+                            <a href="/article/add/" class="btn">Написать в блог</a>
+                        </li>
 
-                            <li class="nav-item dropdown">
+                        <li class="nav-item dropdown">
 
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <img src="/user/getMiniAvatarImage" width="40" height="40"/>
-                                    {{ Auth::user()->nick_name }} <span class="caret"></span>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <img src="/user/getMiniAvatarImage" width="40" height="40"/>
+                                {{ Auth::user()->nick_name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home') }}">Личный кабинет</a>
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Выйти') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('home') }}">Личный кабинет</a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Выйти') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main id="content" class="py-4">
-            @yield('content')
-        </main>
+    <main id="content" class="py-4">
+        @yield('content')
+    </main>
 
 
-        <nav id="footer" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+    <nav id="footer" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
             <ul class="footer-menu">
                 <li>
                     <a href="/">Главная</a>
@@ -128,19 +136,19 @@
                     <a href="#reformal">Отзывы и предложения</a>
                 </li>
                 <li>
-                    <a href="/forum">Форум</a>
+                    <a href="/forums">Форум</a>
                 </li>
             </ul>
 
-                <div id="footer-text">
-                    <p>© {{date("Y")}}, при копировании материалов ссылка обязательна.</p>
-                    <p>Если Вы заметили ошибку на сайте, пожалуйста,
-                        <a href="mailto:falber77@gmail.com">сообщите&nbsp;нам</a>
-                    </p>
-                </div>
+            <div id="footer-text">
+                <p>© {{date("Y")}}, при копировании материалов ссылка обязательна.</p>
+                <p>Если Вы заметили ошибку на сайте, пожалуйста,
+                    <a href="mailto:falber77@gmail.com">сообщите&nbsp;нам</a>
+                </p>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-    </div>
+</div>
 </body>
 </html>
