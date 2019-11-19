49fdf14ab64fa93440d572e22eb0155864c7f20b
tunik <tunik@detali.zp.ua>
2019-11-19 04:09:28 -0500

    мета теги для статей

--- resources/views/article/view.blade.php
+++ resources/views/article/view.blade.php
@@ -2,7 +2,10 @@
 
 @section('head')
 
-    <title>{{$article->title}}</title>
+    <title>{{$article->title}} / Text-уник</title>
+
+    <meta name="description" content="{{$article->meta_description}}">
+    <meta name="keywords" content="{{$article->meta_keywords}}">
 
     <script src="{{ asset('js/comments.js') }}"></script>
 
