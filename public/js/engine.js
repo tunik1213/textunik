f594ebac38a2d81c4cd94807f211fa74e0e70b13
tunik <tunik@detali.zp.ua>
2019-12-17 10:20:57 -0500

    инфо блок

--- public/js/engine.js
+++ public/js/engine.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
 
+    $('.expand-read-more').each(function (i,link) {
+        collapsable = "#"+$(link).attr('for');
+        $(collapsable).hide();
+        $(link).on('click',function () {
+            $("#"+$(this).attr('for')).toggle(200);
+        });
 
-
+    });
 });
