067daf6c0e4c707a247c6a25856c004ab09ac341
tunik <tunik@detali.zp.ua>
2020-02-07 12:21:27 +0200

    исправлено положение картинок в предпросмотре

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -81,7 +81,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body article-text">
 
                 </div>
                 <div class="modal-footer">
