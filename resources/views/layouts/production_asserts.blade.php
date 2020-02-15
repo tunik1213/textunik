cc80dc401fbe40d1342ff944beddbe6a2009dd9e
tunik <tunik@detali.zp.ua>
2020-02-15 10:32:45 +0200

    build frontend

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20200212164528.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20200212164528.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20200215102846.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20200215102846.js') }}{{$gz}}"></script>
