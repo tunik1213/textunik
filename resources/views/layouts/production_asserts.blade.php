0f0ad4bdf0bf9fe6fd3167e086cd322cc977d0a6
tunik <tunik@detali.zp.ua>
2020-02-22 03:47:52 +0200

    размеры шрифтов подзаголовков

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20200221090744.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20200221090744.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20200222034634.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20200222034634.js') }}{{$gz}}"></script>
