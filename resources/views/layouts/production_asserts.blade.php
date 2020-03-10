ca60986ea8729166c696f1fc6e35dd3c682cedaa
tunik <tunik@detali.zp.ua>
2020-03-10 23:02:02 +0200

    убрана уродливая полоса на странице профиля на телефоне

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20200306151753.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20200306151753.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20200310230056.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20200310230056.js') }}{{$gz}}"></script>
