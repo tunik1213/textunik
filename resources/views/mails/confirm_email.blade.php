bb86d3f3a5ecb6e93efd000800c1a4e34b479666
tunik <tunik@detali.zp.ua>
2020-06-04 09:09:07 +0300

    fix

--- resources/views/mails/confirm_email.blade.php
+++ resources/views/mails/confirm_email.blade.php
@@ -6,6 +6,6 @@
 
 <p>Спасибо за регистрацию! Подтвердите, пожалуйста, Ваш email-адрес, который Вы указали при регистрации</p>
 
-<a href="{{ route('confirmEmail', ['Id' => $user->id, 'Token' => $user->api_token]) }}" class="email-action-button">Подтвердить регистрацию</a>
+<a href="{{ route('confirmEmail', ['id' => $user->id, 'token' => $user->api_token]) }}" class="email-action-button">Подтвердить регистрацию</a>
 
 @endsection
