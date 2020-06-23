49acd3c6aed276279039c789e5998820e3b39f29
tunik <tunik@detali.zp.ua>
2020-06-23 16:50:16 +0300

    overflow слишком длинных слов в комментариях чтоб не выползали за край

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20200623113942.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20200623113942.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20200623164814.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20200623164814.js') }}{{$gz}}"></script>
