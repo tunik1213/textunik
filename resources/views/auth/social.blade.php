f819c742b3fb7d8f08d534e93f76d06c272caacd
tunik <tunik@detali.zp.ua>
2020-08-06 12:25:21 +0300

    гугл авторизация

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -2,4 +2,7 @@
     <a class="social-entrance fb" href="{{ route('auth.social', 'facebook') }}" title="Facebook" style="background: #4867aa none repeat scroll 0 0;    color: #fff;">
         Войдите&nbsp;через&nbsp;Facebook
     </a>
+    <a class="social-entrance google" href="{{ route('auth.social', 'google') }}" title="Google" style="background: #ff6550 none repeat scroll 0 0;    color: #fff;">
+        Войдите&nbsp;через&nbsp;Google
+    </a>
 </p>
