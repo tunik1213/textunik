b42d7bb3f4d5baecc177387a8f6de3c6769ca5b3
tunik <tunik@detali.zp.ua>
2020-09-29 15:32:43 +0300

    content

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20200925110740.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20200925110740.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20200929153135.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20200929153135.js') }}{{$gz}}"></script>
