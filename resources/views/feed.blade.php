94b08b7ed93eb894ee271c214792ae8eb97bfbe6
tunik <tunik@detali.zp.ua>
2020-10-01 16:04:28 +0300

    google adsense

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -28,16 +28,18 @@
                     <a href="/about">Подробнее</a>
                 </div>
 
-                <!-- Правый баннер ленты статей -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-9397290056752587"
-                     data-ad-slot="2170069470"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                <div class="right-banner-content">
+                    <!-- Правый баннер ленты статей -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-9397290056752587"
+                         data-ad-slot="2170069470"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
 
                 <div class="right-banner-content">
                     <p class="title-secondary">Популярное</p>
