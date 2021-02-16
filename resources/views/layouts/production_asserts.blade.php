ffff9158b9fc1c53c9d9c2d49e8ffcdb30920354
tunik <tunik@detali.zp.ua>
2021-02-16 16:26:56 +0200

    фикс верстки комментариев

--- resources/views/layouts/production_asserts.blade.php
+++ resources/views/layouts/production_asserts.blade.php
@@ -3,5 +3,5 @@
     $gz = (substr_count($accept_encoding, 'gzip')) ? 'gz' : '';
 @endphp
 
-<link href="{{ asset('build/20210216161539.css') }}{{$gz}}" rel="stylesheet">
-<script src="{{ asset('build/20210216161539.js') }}{{$gz}}"></script>
+<link href="{{ asset('build/20210216162538.css') }}{{$gz}}" rel="stylesheet">
+<script src="{{ asset('build/20210216162538.js') }}{{$gz}}"></script>
